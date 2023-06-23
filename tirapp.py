eaf018cdcdb54f9384808719e7a962a8e5d37cdb
CBB2023 <133326127+CBB2023@users.noreply.github.com>
2023-06-23 17:24:33 +0530

    tirapp.py

--- tirapp.py
+++ tirapp.py
@@ -9,7 +9,7 @@ st.markdown("""
 This app allows you to predict Translation Initiation Rate in Saccharomyces cerevisiae using Machine Learning methods.
 
 **Credits**
-- App built in `Python` + `Streamlit` by Sulagno Chakraborty, Inayat Ullah Irshad, and Dr. Ajeet K. Sharma
+- App built in `Python` + `Streamlit` by Sulagno Chakraborty, Inayat Ullah Irshad, Mahima and Dr. Ajeet K. Sharma
 [[Read the Paper]]().
 ---
 """)
@@ -49,55 +49,109 @@ with open(rf_model_path, 'rb') as f:
 
 # Streamlit app
 def main():
-    
-    # User input
-    option = st.radio("Select option:", ("Enter sequence", "Upload .txt file"))
-    
-    if option == "Enter sequence":
-        sequence = st.text_area("Enter the sequence:")
-        sequences = [sequence]
+    # Title of the dialogue box
+    st.subheader("Enter a gene sequence")
+
+    # User input - Text area for entering the sequence
+    sequence = st.text_area("Sequence")
+
+    # Title for uploading file section
+    st.subheader("Or, upload a file")
+
+    # File upload
+    uploaded_file = st.file_uploader("Upload .txt file", type="txt")
+
+    # Display selected file name or "No file selected"
+    if uploaded_file is not None:
+        file_name = uploaded_file.name
+        st.write("Selected file:", file_name)
     else:
-        uploaded_file = st.file_uploader("Upload .txt file", type="txt")
-        if uploaded_file is not None:
-            content = uploaded_file.read().decode("utf-8")
-            sequences = content.split("\n")
-    
-    if sequences:
-        # Create DataFrame
-        df = pd.DataFrame({'Sequence': sequences})
+        file_name = "No file selected"
 
-        # Calculate Gene Length
-        df['Gene Length'] = df['Sequence'].str.len()
+    # Calculate features and generate dataset button
+    if st.button("Calculate Features and Generate Dataset"):
+        if sequence or uploaded_file:
+            # Create DataFrame
+            if sequence:
+                df = pd.DataFrame({'Sequence': [sequence]})
+            else:
+                content = uploaded_file.read().decode("utf-8")
+                sequences = content.split("\n")
+                df = pd.DataFrame({'Sequence': sequences})
 
-        # Calculate Length of 5' UTR
-        start_codon = 'AUG'
-        df['Length of 5\' UTR'] = df['Sequence'].apply(lambda seq: seq.index(start_codon) if start_codon in seq else 0)
+            # Calculate Gene Length
+            df['Gene Length'] = df['Sequence'].str.len()
 
-        # Calculate Kozak Score
-        kozak_scores = []
-        for sequence in df['Sequence']:
-            kozak_score = calculate_kozak_score(sequence)
-            kozak_scores.append(kozak_score)
-        df['Kozak Score'] = kozak_scores
+            # Calculate Length of 5' UTR
+            start_codon = 'AUG'
+            df['Length of 5\' UTR'] = df['Sequence'].apply(lambda seq: seq.index(start_codon) if start_codon in seq else 0)
 
-        # Define a dictionary that maps each letter to its corresponding value
-        encoding = {"A": 1, "U": 2, "G": 3, "C": 4}
+            # Calculate Kozak Score
+            kozak_scores = []
+            for sequence in df['Sequence']:
+                kozak_score = calculate_kozak_score(sequence)
+                kozak_scores.append(kozak_score)
+            df['Kozak Score'] = kozak_scores
 
-        # Use the map() function to apply the encoding to the first and fourth letters of each string
-        df["First Letter"] = df["Sequence"].str[50-6].map(encoding)
-        df["Fourth Letter"] = df["Sequence"].str[50+3].map(encoding)
+            # Define a dictionary that maps each letter to its corresponding value
+            encoding = {"A": 1, "U": 2, "G": 3, "C": 4}
 
-        # Perform predictions
-        X = df[['Gene Length', 'Length of 5\' UTR', 'Kozak Score', 'First Letter', 'Fourth Letter']]
-        df['Translation Initiation Rate (RF)'] = rf_model.predict(X)
+            # Use the map() function to apply the encoding to the first and fourth letters of each string
+            df["First Letter"] = df["Sequence"].str[50-6].map(encoding)
+            df["Fourth Letter"] = df["Sequence"].str[50+3].map(encoding)
 
-        # Save predictions to CSV
-        df.to_csv('predictions.csv', index=False)
+            # Perform predictions
+            X = df[['Gene Length', 'Length of 5\' UTR', 'Kozak Score', 'First Letter', 'Fourth Letter']]
+            df['Initiation Rate'] = rf_model.predict(X)
 
-        # Display predictions
-        st.write(df)
-    else:
-        st.warning("No sequences found.")
+            # Download dataset
+            csv = df.to_csv(index=False)
+            b64 = base64.b64encode(csv.encode()).decode()
+            href = f'<a href="data:file/csv;base64,{b64}" download="dataset.csv">Download Dataset</a>'
+            st.markdown(href, unsafe_allow_html=True)
+
+    # Prediction button
+    if st.button("PREDICT"):
+        if sequence or uploaded_file:
+            # Create DataFrame
+            if sequence:
+                df = pd.DataFrame({'Sequence': [sequence]})
+            else:
+                content = uploaded_file.read().decode("utf-8")
+                sequences = content.split("\n")
+                df = pd.DataFrame({'Sequence': sequences})
+
+            # Calculate Gene Length
+            df['Gene Length'] = df['Sequence'].str.len()
+
+            # Calculate Length of 5' UTR
+            start_codon = 'AUG'
+            df['Length of 5\' UTR'] = df['Sequence'].apply(lambda seq: seq.index(start_codon) if start_codon in seq else 0)
+
+            # Calculate Kozak Score
+            kozak_scores = []
+            for sequence in df['Sequence']:
+                kozak_score = calculate_kozak_score(sequence)
+                kozak_scores.append(kozak_score)
+            df['Kozak Score'] = kozak_scores
+
+            # Define a dictionary that maps each letter to its corresponding value
+            encoding = {"A": 1, "U": 2, "G": 3, "C": 4}
+
+            # Use the map() function to apply the encoding to the first and fourth letters of each string
+            df["First Letter"] = df["Sequence"].str[50-6].map(encoding)
+            df["Fourth Letter"] = df["Sequence"].str[50+3].map(encoding)
+
+            # Perform predictions
+            X = df[['Gene Length', 'Length of 5\' UTR', 'Kozak Score', 'First Letter', 'Fourth Letter']]
+            df['Initiation Rate'] = rf_model.predict(X)
+
+            # Download predictions
+            csv = df.to_csv(index=False)
+            b64 = base64.b64encode(csv.encode()).decode()
+            href = f'<a href="data:file/csv;base64,{b64}" download="predictions.csv">Download Predictions</a>'
+            st.markdown(href, unsafe_allow_html=True)
+
 
 if __name__ == "__main__":
     main()

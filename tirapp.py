4f0d678cfc6e6a0e2ffee8695884a5aaea493948
CBB2023 <133326127+CBB2023@users.noreply.github.com>
2023-07-13 22:15:41 +0530

    tirapp.py

--- tirapp.py
+++ tirapp.py
@@ -5,7 +5,30 @@ import RNA
 import pickle
 import features
 
+def evaluate_model(model, X_test):
+    y_pred = model.predict(X_test)
+    return y_pred
 
+
+def main():
+  gene_sequence = st.text_input("Enter the mRNA sequence:")
+  start_codon = st.number_input("Enter the start codon position:")
+  stop_codons = st.text_input("Enter the stop codons:")
+
+  if st.button("Calculate features"):
+    start_codon_index = int(start_codon)
+
+    gene_features = features.features(gene_sequence, start_codon_index, stop_codons)
+    df = pd.DataFrame(gene_features, index=["Value"])
+    st.subheader("Calculated Features:")
+    st.write(df)
+
+if __name__ == "__main__":
+    main()
+
+
+
+"""
 def evaluate_model(model, X_test):
     y_pred = model.predict(X_test)
     return y_pred
@@ -36,3 +59,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+"""

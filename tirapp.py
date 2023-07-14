817ab411ae78de66d97e3b040be037159a9cd9f5
CBB2023 <133326127+CBB2023@users.noreply.github.com>
2023-07-14 13:15:34 +0530

    tirapp.py

--- tirapp.py
+++ tirapp.py
@@ -25,38 +25,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-"""
-def evaluate_model(model, X_test):
-    y_pred = model.predict(X_test)
-    return y_pred
-    
-    
-def main():
-  gene_sequence = st.text_input("Enter the mRNA sequence:")
-  start_codon = st.number_input("Enter the start codon position:")
-  stop_codons = st.text_input("Enter the stop codons:")
-
-  if st.button("Calculate features"):
-    
-    gene_features = features.features(gene_sequence, start_codon, stop_codons)
-    df = pd.DataFrame(gene_features, index=["Value"])
-    st.subheader("Calculated Features:")
-    st.write(df)
-
-# Start Prediction Button
-  if st.button("Start Prediction"):  
-        # Load Models
-        rf_model_path = "tir_rf_model.pkl"
-
-        with open(rf_model_path, 'rb') as f:
-            rf_model = pickle.load(f)
-
-        # Evaluate Random Forest Model
-        rf_y_pred = evaluate_model(rf_model, df)
-
-if __name__ == "__main__":
-    main()
-"""
